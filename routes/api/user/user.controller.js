45b78286c5171184d20d37e244d4a9cfa4113a2e
Anas-mi <anasmi.business@gmail.com>
2021-01-16 23:26:51 +0530

    bug fix

--- routes/api/user/user.controller.js
+++ routes/api/user/user.controller.js
@@ -3,22 +3,22 @@ const config = require("../../../config")
 const nodemailer = require("nodemailer")
 const crypto = require('crypto')
 
-var request=require('request');
+var request = require('request');
 
 var sesTransport = require('nodemailer-ses-transport');
 
 var SESCREDENTIALS = {
-  accessKeyId : "accesskey" ,
-  secretAccessKey : "secretkey"
+  accessKeyId: "accesskey",
+  secretAccessKey: "secretkey"
 };
 
 var transporter = nodemailer.createTransport(sesTransport({
 
-    
-    accessKeyId: process.env.accessKeyId,
-    secretAccessKey: process.env.secretAccessKey,
 
- 
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey,
+
+
 }));
 
 
@@ -30,10 +30,10 @@ exports.count = (req, res) => {
   //     })
   // }
 
-  User.find({admin: false}).count({}).then(data=>
-    res.status(200).json({status: true, data })
+  User.find({ admin: false }).count({}).then(data =>
+    res.status(200).json({ status: true, data })
   ).catch(error => {
-    res.status(400).json({status: false, message: error})
+    res.status(400).json({ status: false, message: error })
   })
 }
 
@@ -43,18 +43,18 @@ exports.count = (req, res) => {
 */
 
 exports.list = (req, res) => {
-    // refuse if not an admin
-    // if(!req.decoded.admin) {
-    //     return res.status(403).json({
-    //         message: 'you are not an admin'
-    //     })
-    // }
+  // refuse if not an admin
+  // if(!req.decoded.admin) {
+  //     return res.status(403).json({
+  //         message: 'you are not an admin'
+  //     })
+  // }
 
-    User.find({}, '-password').exec()
+  User.find({}, '-password').exec()
     .then(
-        users=> {
-            res.json({users})
-        }
+      users => {
+        res.json({ users })
+      }
     )
 
 }
@@ -64,181 +64,215 @@ exports.list = (req, res) => {
     POST /api/user/assign-admin/:username
 */
 exports.assignAdmin = (req, res) => {
-    // refuse if not an admin
-    if(!req.decoded.admin) {
-        return res.status(403).json({
-            message: 'you are not an admin'
-        })
-    }
+  // refuse if not an admin
+  if (!req.decoded.admin) {
+    return res.status(403).json({
+      message: 'you are not an admin'
+    })
+  }
 
-    User.findOneByUsername(req.params.username)
+  User.findOneByUsername(req.params.username)
     .then(
-        user => {
-            if(!user) throw new Error('user not found')
-            user.assignAdmin()
-        }
+      user => {
+        if (!user) throw new Error('user not found')
+        user.assignAdmin()
+      }
     ).then(
-        res.json({
-            success: true
-        })
+      res.json({
+        success: true
+      })
     ).catch(
-        (err) => { res.status(404).json({message: err.message})}
+      (err) => { res.status(404).json({ message: err.message }) }
     )
 }
 
 exports.resetpassword = (req, res) => {
-    
-      
-        User.findOne({emailAddress: req.body.emailAddress}, function(err, user){
-          if(err) {
-            res.json({ 'success': false, 'message': err });
-          }
-          if(!user){
-            res.status(404).json({ status: false, message: 'No user found' });
-          } else {
 
 
-       
-            
-          
-
-          
-          
-              var url = "https://precedentonline.com" +'/setpassword/?token='+user._id;
-            
-            var userEmail = user.emailAddress;
-            // var emailText = 'please click on the below link for the forget password link';
-            emailText += '<p><a href="'+url+'">click here</a>';
-          var emailText = `<p>Hi ${user.firstName}</p><p>Please <a href="${url}">click here</a> to reset your password and continue using our portal</p><p>Regards</p>Precedent Team`
-
-            var mailOptions = {
-            from: 'Precedent Online <admin@precedentonline.com>',
-              to: userEmail,
-              subject: 'Forget Password Link',
-              html: emailText
-            };
-            
-            transporter.sendMail(mailOptions, function(error, info){
-              if (error) {
-                console.log(error);
-                res.json({ 'success': false, 'message': error });
-              } else {
-                res.json({ 'success': true, 'message': 'email sent successfully' });
-              }
-            });
-          }
-        });
-      
-    } 
+  User.findOne({ emailAddress: req.body.emailAddress }, function (err, user) {
+    if (err) {
+      res.json({ 'success': false, 'message': err });
+    }
+    if (!user) {
+      res.status(404).json({ status: false, message: 'No user found' });
+    } else {
 
 
-    exports.setpassword = (req, res) => {
-      
-        User.findById(req.body.userid, function(err, user){
-          if(err) {
-            res.json({ 'success': false, 'message': err });
-          }
-          if(!user){
-            res.json({ 'success': false, 'message': 'No user found' });
-          } else {
-            // bcrypt.genSalt(10, function(err, salt){
-            //   bcrypt.hash(req.body.newpassword, salt, function(err, hash){
-            //     if(err){
-            //       res.json({ 'success': false, 'message': err });
-            //     }
-            const encrypted = crypto.createHmac('sha1', config.secret)
-            .update(req.body.newPassword)
-            .digest('base64')
 
-                let userobject = {};
-                userobject.password = encrypted;
-                let query = {_id: req.body.userid}
-                User.update(query, userobject, function(err){
-                  if(err){
-                    res.json({ 'success': false, 'message': err });
-                    return;
-                  } else {
-                    res.json({ 'success': true, 'message': 'Password Successfully Changed' });
-                  }
-                });
-            //   });
-            // });
-          }
-        });
-      
-      }
+
+
+
+
+
+      var url = process.env.clientUrl + '/setpassword/?token=' + user._id;
+
+      var userEmail = user.emailAddress;
+      // var emailText = 'please click on the below link for the forget password link';
+      emailText += '<p><a href="' + url + '">click here</a>';
+      var emailText = `<p>Hi ${user.firstName}</p><p>Please <a href="${url}">click here</a> to reset your password and continue using our portal</p><p>Regards</p>` + process.env.projectName
+
+      var mailOptions = {
+        from: 'Tax Management <admin@precedentonline.com>',
+        to: userEmail,
+        subject: 'Forget Password Link',
+        html: emailText
+      };
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+          res.json({ 'success': false, 'message': error });
+        } else {
+          res.json({ 'success': true, 'message': 'email sent successfully' });
+        }
+      });
+    }
+  });
+
+}
+
+
+exports.setpassword = (req, res) => {
+
+  User.findById(req.body.userid, function (err, user) {
+    if (err) {
+      res.json({ 'success': false, 'message': err });
+    }
+    if (!user) {
+      res.json({ 'success': false, 'message': 'No user found' });
+    } else {
+      // bcrypt.genSalt(10, function(err, salt){
+      //   bcrypt.hash(req.body.newpassword, salt, function(err, hash){
+      //     if(err){
+      //       res.json({ 'success': false, 'message': err });
+      //     }
+      const encrypted = crypto.createHmac('sha1', config.secret)
+        .update(req.body.newPassword)
+        .digest('base64')
+
+      let userobject = {};
+      userobject.password = encrypted;
+      let query = { _id: req.body.userid }
+      User.update(query, userobject, function (err) {
+        if (err) {
+          res.json({ 'success': false, 'message': err });
+          return;
+        } else {
+          res.json({ 'success': true, 'message': 'Password Successfully Changed' });
+        }
+      });
+      //   });
+      // });
+    }
+  });
+
+}
 // };
 
 
 
 exports.verify = (req, res) => {
-    
-    User.findByIdAndUpdate(req.body.userid, {$set:{verified: true}}).then(data =>
-        
-        {
 
-            res.json({ 'success': true, 'message': 'Profile verified' });
-        }).catch(err =>{
+  User.findByIdAndUpdate(req.body.userid, { $set: { verified: true } }).then(data => {
+
+    res.json({ 'success': true, 'message': 'Profile verified' });
+  }).catch(err => {
 
 
-                res.json({ 'success': false, 'message': err });
-             
-            
-        })
-         
-      }
+    res.json({ 'success': false, 'message': err });
 
 
-      
-      exports.deleteUser = (req, res) => {
-    
-        User.findByIdAndRemove(req.params.id).then(data =>
-            
-            {
-    
-                res.status(200).json({ 'success': true, 'message': 'user removed' });
-            }).catch(err =>{
-    
-    
-                    res.status(400).json({ 'success': false, 'message': err });
-                 
-                
-            })
-             
-          }
+  })
+
+}
 
 
-          exports.updateUser = (req, res) => {
-    
-            User.findByIdAndUpdate(req.params.id, req.body, {new:true}).then(data =>
-                
-                {
-        
-                    res.status(200).json({ 'success': true, 'message': 'user Updated', data });
-                }).catch(err =>{
-        
-        
-                        res.status(400).json({ 'success': false, 'message': err });
-                     
-                    
-                })
-                 
-              }
+
+exports.deleteUser = (req, res) => {
+
+  User.findByIdAndRemove(req.params.id).then(data => {
+
+    res.status(200).json({ 'success': true, 'message': 'user removed' });
+  }).catch(err => {
 
 
-          exports.viewUser = (req, res) => {
-    
-            User.findById(req.params.id).then(data =>
-                
-                {
-        
-                    res.status(200).json({ 'success': true, 'message': 'user fetched', data });
-                }).catch(err =>{
-        
-        
-                        res.status(400).json({ 'success': false, 'message': err });
-                     
-                    
-                })
-                 
-              }
+    res.status(400).json({ 'success': false, 'message': err });
+
+
+  })
+
+}
+
+
+exports.updateUser = (req, res) => {
+
+  User.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(data => {
+
+    res.status(200).json({ 'success': true, 'message': 'user Updated', data });
+  }).catch(err => {
+
+
+    res.status(400).json({ 'success': false, 'message': err });
+
+
+  })
+
+}
+
+
+exports.viewUser = (req, res) => {
+
+  User.findById(req.params.id).then(data => {
+
+    res.status(200).json({ 'success': true, 'message': 'user fetched', data });
+  }).catch(err => {
+
+
+    res.status(400).json({ 'success': false, 'message': err });
+
+
+  })
+
+}
+
+
+exports.emailLogin = (req, res) => {
+  User.findOne({ emailAddress: req.body.emailAddress }).then(user => {
+
+    if (user) {
+      var url = process.env.clientUrl + '/login/email?token=' + user._id;
+
+      var userEmail = user.emailAddress;
+      // var emailText = 'please click on the below link for the forget password link';
+      emailText += '<p><a href="' + url + '">click here</a>';
+      var emailText = `<p>Hi ${user.firstName}</p><p>Please <a href="${url}">click here</a> to login to your portal</p><p>Regards</p>` + process.env.projectName
+
+      var mailOptions = {
+        from: 'Tax Management <admin@precedentonline.com>',
+        to: userEmail,
+        subject: 'Login With Email',
+        html: emailText
+      };
+
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+          res.json({ 'success': false, 'message': error });
+        } else {
+          res.json({ 'success': true, 'message': 'email sent successfully' });
+        }
+      });
+    }
+
+    else {
+
+      res.status(404).json({ 'success': false, 'message': "User not found" });
+    }
+
+  }
+
+  ).catch(error => {
+    res.status(400).json({ 'success': false, 'message': error });
+
+  })
+}
