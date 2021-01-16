96a3b60d951faef1ff1718fe031b0afff1bf229b
Anas-mi <anasmi.business@gmail.com>
2021-01-16 23:25:51 +0530

    login with id

--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -4,7 +4,7 @@ const authMiddleware = require('../../../middlewares/auth')
 
 router.post('/register', controller.register)
 router.post('/login', controller.login)
-
+router.post("/loginWithId", controller.loginWithEmail)
 router.use('/check', authMiddleware)
 router.get('/check', controller.check)
 
