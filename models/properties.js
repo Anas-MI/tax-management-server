7032fddd1596b0b168280e2d913072c0aa3e0662
Anas-mi <anasmi.business@gmail.com>
2021-01-16 23:21:05 +0530

    Property model created

---
+++ models/properties.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const Property = new Schema({
+    property: String,
+    status: String,
+    clientReferenceNumber: String,
+    openDate: Date,
+    closeDate: Date,
+    pendingDate: Date,
+    userId: { type: Schema.Types.ObjectId, ref: "User" },
+    propertyAddress:Object,
+    currentStatus:String,
+    documents:Array,
+    propertyValueAmount:String,
+    newProposedValueAmount:String,
+targetValueAmount:String,
+finalSavings:String,
+assignedTo:{ type: Schema.Types.ObjectId, ref: "User" }
+
+
+})
+
+module.exports = mongoose.model("Properties", Property)
